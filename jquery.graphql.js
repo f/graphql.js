0d0a252f73ed63bd72350967586530a71e42931b
Fatih Kadir Akın <f@users.noreply.github.com>
2017-02-10 10:27:59 +0200

    initial

---
+++ jquery.graphql.js
@@ -0,0 +1,37 @@
+!(function ($) {
+  $.graphql = function (url, headers, fragments) {
+    if (!fragments) fragments = {}
+    var fragmentObject = {}
+    $.each(fragments, function (name, fragment) {
+      fragmentObject[name] = "fragment " + name + " " + fragment
+    })
+    function processQuery(query) {
+      var fragmentRegexp = /\.\.\.\s*([A-Za-z0-9]+)/g
+      var usedFragments = $.map(query.match(fragmentRegexp), function (fragment) {
+        return fragmentObject[fragment.replace(fragmentRegexp, function (_, $m) {return $m})]
+      })
+      return [query].concat(usedFragments).join("\n")
+    }
+    return function (query, variables, overrideHeaders) {
+      var defer = $.Deferred()
+      var fragmentedQuery = processQuery(query)
+      $.ajax({
+        type: "post",
+        url: url,
+        headers: $.extend(headers, overrideHeaders),
+        dataType: "json",
+        data: {query: fragmentedQuery, variables: variables},
+        success: function (response) {
+          if (response.errors) {
+            defer.reject(response.errors)
+          } else if (response.data) {
+            defer.resolve(response.data)
+          } else {
+            defer.reject(response)
+          }
+        }
+      })
+      return defer
+    }
+  }
+(jQuery))
