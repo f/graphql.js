9dad9abc785fd6b15e3a8c012bee2e3de37340db
Edward Rudd <urkle@outoforder.cc>
2021-12-14 21:46:54 -0500

    switch test suite to jest

--- test/test.js
+++ test/test.js
@@ -1,38 +1,65 @@
-var assert = require('assert')
-var graphql = require('../graphql.js')
+let graphql = require('../graphql.js');
 
-var client = graphql(null, {
-	method: 'put',
-	fragments: {
-		user: 'on User {name}',
-		auth: {
-			user: 'on User {token, ...user}'
-		}
-	}
-})
+describe('graphql.js', () => {
+    let client = null;
 
-client.fragment({
-	auth: {
-		error: 'on Error {messages}'
-	}
-})
-assert.equal(typeof client, 'function')
-assert.equal(
-	client.fragment('auth.error'),
-	'fragment auth_error on Error {messages}'
-)
-assert.equal(client.getOptions().method, 'put')
-assert.equal(client.fragments().user, '\nfragment user on User {name}')
-assert.equal(
-	client.fragments().auth_user,
-	'\nfragment auth_user on User {token, ...user}'
-)
-assert.equal(
-	client.fragments().auth_error,
-	'\nfragment auth_error on Error {messages}'
-)
+    beforeEach(() => {
+        client = graphql(null, {
+            method: 'put',
+            fragments: {
+                user: 'on User {name}',
+                auth: {
+                    user: 'on User {token, ...user}'
+                }
+            }
+        });
+        client.fragment({
+            auth: {
+                error: 'on Error {messages}'
+            }
+        });
+    });
 
-var queryIn = `query (@autodeclare) {
+    it('client should be a function', () => {
+        expect(typeof client).toBe('function');
+    });
+
+    describe('.fragment()', () => {
+        it('registers a new fragment', () => {
+
+            expect(client.fragment('auth.error')).toBe(
+                'fragment auth_error on Error {messages}'
+            );
+        });
+    });
+
+    describe('.getOptions()', () => {
+        it('configures the method as requested', () => {
+            expect(client.getOptions().method).toBe('put');
+        });
+    });
+
+    describe('.fragments()', () => {
+        it('returns an object with the defined fragments as properties', () => {
+            expect(client.fragments()).toStrictEqual(
+                expect.objectContaining({
+                    user: '\nfragment user on User {name}',
+                    auth_user: '\nfragment auth_user on User {token, ...user}',
+                })
+            );
+        });
+
+        it('returns returns new fragments registered as well', () => {
+            expect(client.fragments()).toStrictEqual(
+                expect.objectContaining({
+                    auth_error: '\nfragment auth_error on Error {messages}',
+                })
+            );
+        });
+    });
+
+    describe('@autodeclare queries', () => {
+        let queryIn = `query (@autodeclare) {
 	user(name: $name, bool: $bool, int: $int, id: $id) {
 		...auth.user
 		...auth.error
@@ -40,9 +67,10 @@ var queryIn = `query (@autodeclare) {
 	x {
 		... auth.user
 	}
-}`
+}`;
 
-var expectedQuery = `query ($name: String!, $bool: Boolean!, $int: Int!, $float: Float!, $id: ID!, $user_id: Int!, $postID: ID!, $custom_id: CustomType!, $customId: ID!, $target: [ID!]!) {
+        it('mixes in the requested fragments and sets the data types', () => {
+            var expectedQuery = `query ($name: String!, $bool: Boolean!, $int: Int!, $float: Float!, $id: ID!, $user_id: Int!, $postID: ID!, $custom_id: CustomType!, $customId: ID!, $target: [ID!]!) {
 	user(name: $name, bool: $bool, int: $int, id: $id) {
 		... auth_user
 		... auth_error
@@ -56,56 +84,60 @@ fragment user on User {name}
 
 fragment auth_user on User {token, ...user}
 
-fragment auth_error on Error {messages}`
+fragment auth_error on Error {messages}`;
 
-assert.equal(
-	client.buildQuery(queryIn, {
-		name: 'fatih',
-		bool: true,
-		int: 2,
-		float: 2.3,
-		id: 1,
-		'user_id!': 2,
-		'postID': '45af67cd',
-		'custom_id!CustomType': '1',
-		'customId': '1',
-		'target![ID!]': ['Q29uZ3JhdHVsYXRpb25z']
-	}),
-	expectedQuery
-)
+            let query = client.buildQuery(queryIn, {
+                name: 'fatih',
+                bool: true,
+                int: 2,
+                float: 2.3,
+                id: 1,
+                'user_id!': 2,
+                'postID': '45af67cd',
+                'custom_id!CustomType': '1',
+                'customId': '1',
+                'target![ID!]': ['Q29uZ3JhdHVsYXRpb25z']
+            });
 
-assert.equal(
-	typeof client.query(`($email: String!, $password: String!) {
-		auth(email: $email, password: $password) {
-			... on User {
-				token
-			}
-		}
-	}`),
-	'function'
-)
+            expect(query).toBe(expectedQuery);
+        });
+    });
 
-/**
-* URL UPDATE TESTING
-*/
+    describe('.query()', () => {
+        it('returns a function', () => {
+            let query = client.query(`($email: String!, $password: String!) {
+                auth(email: $email, password: $password) {
+                    ... on User {
+                        token
+                    }
+                }
+            }`);
+            expect(typeof query).toBe('function');
+        });
+    });
 
-client.headers({ 'User-Agent': 'Awesome-Octocat-App' })
-var query = client.query(`
-repository(owner:"f", name:"graphql.js") {
-	issues(last:20, states:CLOSED) {
-		edges {
-			node {
-				title
-				url
-			}
-		}
-	}
-}`)
+    describe('.getUrl()/setUrl()', () => {
+        it('updates the url', () => {
+            client.headers({'User-Agent': 'Awesome-Octocat-App'});
 
-// Checking Old URL
-assert.equal(client.getUrl(), null)
+            client.query(`
+                repository(owner:"f", name:"graphql.js") {
+                    issues(last:20, states:CLOSED) {
+                        edges {
+                            node {
+                                title
+                                url
+                            }
+                        }
+                    }
+                }`);
 
-// Checking New URL
-var newUrl = 'https://api.github.com/graphql'
-client.setUrl(newUrl)
-assert.equal(client.getUrl(), newUrl)
+            // check old URL
+            expect(client.getUrl()).toBeNull();
+            // set new URL
+            var newUrl = 'https://api.github.com/graphql'
+            client.setUrl(newUrl)
+            expect(client.getUrl()).toBe(newUrl);
+        })
+    });
+});
